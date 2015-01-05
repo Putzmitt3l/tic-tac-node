cd27ba205cc7a00881e49bc293e5a73928b77ca3
dobata <dkralev90@gmail.com>
2015-01-05 14:07:07 +0200

    Rewriting assessment functions

--- server/assessment.js
+++ server/assessment.js
@@ -4,16 +4,13 @@ function checkForWin (board) {
     for (var i = 0; i < boardDimension; i++) {
         if(checkRow(board, i, board[i][0]) === 3 &&
             board[i][0] !== 0) {
-
-            result.isWon = true;
-            result.winner = (board[i][0] === 1)? 'x' : 'o';
+            attachWin(result, board[i][0]);
             break;
         }
         else if(checkCol(board, i, board[0][i]) === 3 &&
             board[0][i] !== 0) {
 
-            result.isWon = true;
-            result.winner = (board[0][i] === 1)? 'x' : 'o';
+            attachWin(result, board[0][i]);
             break;
         }
     };
@@ -21,54 +18,47 @@ function checkForWin (board) {
     if(checkDiagonal(board, board[0][0]) === 3 &&
         board[0][0] !== 0) {
 
-        result.isWon = true;
-        result.winner = (board[0][0] === 1)? 'x' : 'o';
+        attachWin(result, board[0][0]);
     }
-    else if(checkDiagonal(board, board[0][1], true) === 3 &&
-        board[0][1] !== 0) {
+    else if(checkDiagonal(board, board[0][2], true) === 3 &&
+        board[0][2] !== 0) {
 
-        result.isWon = true;
-        result.winner = (board[0][1] === 1)? 'x' : 'o';
+        attachWin(result, board[0][2]);
     }
 
     return result;
 };
 
+function attachWin(result, cell) {
+    result.isWon = true;
+    result.winner = (cell === 1)? 'x' : 'o';
+}
+
 function checkRow (board, row, cell) {
-    return check(board, row, cell)
+    var sameAsCellCounter = 0;
+    for (var i = 0; i < 3; i++) {
+        if(board[row][i] === cell) {
+            sameAsCellCounter++;
+        }
+        else if(board[row][i] !== cell && board[row][i] !== 0) {
+            sameAsCellCounter--;
+        }
+    };
+    return sameAsCellCounter;
 };
 
 function checkCol (board, col, cell) {
-    return check(board, col, cell, true);
+    var sameAsCellCounter = 0;
+    for (var i = 0; i < 3; i++) {
+        if(board[i][col] === cell) {
+            sameAsCellCounter++;
+        }
+        else if(board[i][col] !== cell && board[i][col] !== 0) {
+            sameAsCellCounter--;
+        }
+    };
+    return sameAsCellCounter;
 };
-
-function check (board, index, cell, isCol) {
-    var euriCheckVal = 0;
-    if(!!isCol) {
-        // we are checking a row
-        for (var i = 0; i < 3; i++) {
-            if (board[index][i] === cell) {
-                euriCheckVal++;
-            }
-            else if(board[index][i] !== cell && board[index][i] !== 0) {
-                euriCheckVal--;
-            }
-        };
-    }
-    else {
-        // we are checking a column
-        for (var i = 0; i < 3; i++) {
-            if (board[i][index] === cell) {
-                euriCheckVal++;
-            }
-            else if(board[i][index] !== cell && board[i][index] !== 0) {
-                euriCheckVal--;
-            }
-        };
-    }
-    // possible return values in interval [-2, 2]
-    return euriCheckVal;
-}
 
 function checkDiagonal (board, cell, checkOpposite) {
     var euriCheckVal = 0;
@@ -82,7 +72,6 @@ function checkDiagonal (board, cell, checkOpposite) {
                 euriCheckVal--;
             }
         };
-
     }
     else {
         // check reverse diagonal
